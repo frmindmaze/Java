834cf5a2bcb69777e00646f18dccc76f0bebbb23
frmindmaze <43980482+frmindmaze@users.noreply.github.com>
2021-07-12 14:00:53 +0530

    Add files via upload

---
+++ merge.ps1
@@ -0,0 +1,8 @@
+﻿$sideacsv= import-csv -Path D:\Sidea
+$sidebcsv = Import-Csv -Path D:\Sideb
+$sidebfinalcsv= 'D:merged\final.csv'
+$CSV = Get-ChildItem -Path $sidebcsv -Filter *.csv | ForEach-Object { 
+    Import-Csv -Path $_
+}
+
+$CSV | Export-Csv -Path $sidebfinalcsv -NoTypeInformation -Force;
